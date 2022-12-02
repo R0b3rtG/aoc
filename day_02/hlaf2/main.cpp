2ffc82fe1d6fb02f6f55e2ef651f38166cff9b6c
R0b3rtG <robertiancu24@gmail.com>
2022-12-02 08:02:22 +0200

    day_02 solutions

---
+++ day_02/hlaf2/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+ifstream f("input.txt");
+
+int main()
+{
+    int s = 0;
+    char a,b;
+    while(f>>a>>b)
+    {
+        if(a == 'A' && b == 'X')
+            s+= 3;
+        if(a == 'B' && b == 'X')
+            s+= 1;
+        if(a == 'C' && b == 'X')
+            s+= 2;
+        if(a == 'A' && b == 'Y')
+            s+= 4;
+        if(a == 'B' && b == 'Y')
+            s+= 5;
+        if(a == 'C' && b == 'Y')
+            s+= 6;
+        if(a == 'A' && b == 'Z')
+            s+= 8;
+        if(a == 'B' && b == 'Z')
+            s+= 9;
+        if(a == 'C' && b == 'Z')
+            s+= 7;
+    }
+
+    cout << "Your score is: " << s;
+    return 0;
+}
---
+++ file_reading_boiler_plate.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	console.log(data);
+});
