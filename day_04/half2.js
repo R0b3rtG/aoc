34dd779f7d9f36996d4ad58ec0bfe83f475a54e6
R0b3rtG <robertiancu24@gmail.com>
2022-12-04 11:03:43 +0200

    day_04 solutions

---
+++ day_04/half2.js
@@ -0,0 +1,31 @@
+let fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	let pairs = data.split('\r\n');
+
+	let numOfOverlappingRanges = 0;
+	pairs.forEach((pair) => {
+		let ranges = pair.split(',');
+		let rangeEnds = [...ranges[0].split('-'), ...ranges[1].split('-')];
+		if (
+			(parseInt(rangeEnds[0]) >= parseInt(rangeEnds[2]) &&
+				parseInt(rangeEnds[0]) <= parseInt(rangeEnds[3])) ||
+			(parseInt(rangeEnds[1]) >= parseInt(rangeEnds[2]) &&
+				parseInt(rangeEnds[1]) <= parseInt(rangeEnds[3])) ||
+			(parseInt(rangeEnds[2]) >= parseInt(rangeEnds[0]) &&
+				parseInt(rangeEnds[2]) <= parseInt(rangeEnds[1])) ||
+			(parseInt(rangeEnds[3]) >= parseInt(rangeEnds[0]) &&
+				parseInt(rangeEnds[3]) <= parseInt(rangeEnds[1]))
+		) {
+			console.log(rangeEnds);
+			numOfOverlappingRanges++;
+		}
+	});
+
+	console.log(numOfOverlappingRanges);
+});
